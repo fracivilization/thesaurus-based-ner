becc36d21612705340a139d501e85a30289db603
takayoshi-s <fractal.civilization@gmail.com>
2022-07-31 17:13:03 +0900

    gold msmlc datasetの修正に合わせて np ratioを変更した

--- scripts/search_params/negative_positive_ratio_on_gold.sh
+++ scripts/search_params/negative_positive_ratio_on_gold.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir=`dirname $0`
-negative_ratios=(1.0 4.0 16.0 64.0 128.0)
+negative_ratios=(0.1 0.5 1,0)
 # negative_ratios=(32.0 64.0 128.0)
 for negative_ratio in ${negative_ratios[@]}; do
     MAKE="NEGATIVE_CATS=\"\" MSMLC_PN_RATIO_EQUIVALENCE=True MSMLC_NEGATIVE_RATIO_OVER_POSITIVE=${negative_ratio} make"
