ecc28494b71ca7f2152e4bd89b3ce4c8889a1e87
takayoshi-s <fractal.civilization@gmail.com>
2022-09-17 22:36:51 +0900

    CPU/GPUサーバー用のproxy設定を追加

---
+++ scripts/raiden_cpu_server_proxy.sh
@@ -0,0 +1,7 @@
+export MY_PROXY_URL=http://10.1.30.1:8080/
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
