ae3eaf82eb67a2cf3282c71aada2bf6df501a49b
takayoshi-s <fractal.civilization@gmail.com>
2022-09-24 19:40:10 +0900

    単複形の追加で曖昧性が付与される事例に対し追加を行わないようにした

--- cli/preprocess/load_term2cats.py
+++ cli/preprocess/load_term2cats.py
@@ -18,14 +18,22 @@ register_term2cat_configs(None)
 def main(config: Term2CatsConfig):
     output_path = os.path.join(get_original_cwd(), config.output)
     term2cats = load_term2cats(config)
-    
-    print('load inflected term2cats')
+
+    print("load inflected term2cats")
     count_inflected_term2cats = dict()
     for term, cats in tqdm(term2cats.items()):
-        sigularized_term = singularize(term)
+        singularized_term = singularize(term)
         pluralized_term = pluralize(term)
-        count_inflected_term2cats[sigularized_term] = cats
-        count_inflected_term2cats[pluralized_term] = cats
+
+        expanded_terms = set()
+        for new_term in [singularized_term, pluralized_term]:
+            if new_term != term:
+                expanded_terms.add(pluralized_term)
+
+        count_inflected_term2cats[term] = cats
+        for expanded_term in expanded_terms:
+            if expanded_term not in term2cats:
+                count_inflected_term2cats[expanded_term] = cats
 
     with open(output_path, "wb") as f:
         pickle.dump(count_inflected_term2cats, f)
