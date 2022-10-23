3b3c10b693e6df46747831d4170f2acf65679d18
takayoshi-s <fractal.civilization@gmail.com>
2022-10-23 20:00:12 +0900

    makefileのNEATIVE_CATSを自動で決定するようにした

--- cli/preprocess/load_negative_categories.py
+++ cli/preprocess/load_negative_categories.py
@@ -4,9 +4,13 @@ import click
 
 @click.command()
 @click.option("--focus-categories", type=str)
-def main(focus_categories: str):
-    focus_categories = focus_categories.split(CATEGORY_SEPARATOR)
-    print("_".join(get_umls_negative_cats_from_focus_cats(focus_categories)))
+@click.option("--with-negative_categories", type=bool)
+def main(focus_categories: str, with_negative_categories: bool):
+    if with_negative_categories:
+        focus_categories = focus_categories.split(CATEGORY_SEPARATOR)
+        print(" ".join(get_umls_negative_cats_from_focus_cats(focus_categories)))
+    else:
+        print("")
 
 
 if __name__ == "__main__":
