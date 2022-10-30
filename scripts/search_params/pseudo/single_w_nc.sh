e4d27e645c644ba5c6ce97d31465d462491d9de9
takayoshi-s <fractal.civilization@gmail.com>
2022-10-30 21:21:57 +0900

    RAIDENでも疑似データのハイパラ探索ができるようにした

--- scripts/search_params/pseudo/single_w_nc.sh
+++ scripts/search_params/pseudo/single_w_nc.sh
@@ -3,7 +3,7 @@
 #$ -jc gpu-container_g4
 #$ -ac d=nvcr-pytorch-2205
 dir=`dirname $0`
-OUTPUT_DIR=outputs/search_params/pseudo/multi
+OUTPUT_DIR=outputs/search_params/pseudo/single_w_nc
 mkdir -p ${OUTPUT_DIR}
 pwd >> ${OUTPUT_DIR}/cout
 ls -la >> ${OUTPUT_DIR}/cout
@@ -16,9 +16,9 @@ export http_proxy=$MY_PROXY_URL
 export https_proxy=$MY_PROXY_URL
 export ftp_proxy=$MY_PROXY_URL
 
-negative_ratios=(0.5 0.3 0.1 0.05 0.01 0.005)
+negative_ratios=(0.02 0.03 0.04 0.05)
 for negative_ratio in ${negative_ratios[@]}; do
     echo "negative_ratio: ${negative_ratio}" >>${OUTPUT_DIR}/cout
-    MAKE="MSMLC_DYNAMIC_PN_RATIO_EQUIVALENCE=True MSMLC_NEGATIVE_RATIO_OVER_POSITIVE=${negative_ratio} make"
-    eval ${MAKE} eval_flatten_marginal_softmax -j$(nproc) >>${OUTPUT_DIR}/cout 2>>${OUTPUT_DIR}/cerr
+    MAKE="WITH_NEGATIVE_CATEGORIES=True NEGATIVE_RATIO_OVER_POSITIVE=${negative_ratio} WITH_O=True FIRST_STAGE_CHUNKER=\"enumerated\" POSITIVE_RATIO_THR_OF_NEGATIVE_CAT=1.0 make"
+    eval ${MAKE} train -j$(nproc) >>${OUTPUT_DIR}/cout 2>>${OUTPUT_DIR}/cerr
 done
