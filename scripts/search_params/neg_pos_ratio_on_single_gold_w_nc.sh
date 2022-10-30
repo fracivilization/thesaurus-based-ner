abaad780de4ad52ed4626e6b1774edf198a538b5
takayoshi-s <fractal.civilization@gmail.com>
2022-10-30 20:40:50 +0900

    fix: Raiden上で動くscriptになるように変更した (Goldデータ編)

--- scripts/search_params/neg_pos_ratio_on_single_gold_w_nc.sh
+++ scripts/search_params/neg_pos_ratio_on_single_gold_w_nc.sh
@@ -3,7 +3,7 @@
 #$ -jc gpu-container_g4
 #$ -ac d=nvcr-pytorch-2205
 dir=`dirname $0`
-OUTPUT_DIR=outputs/search_params/neg_pos_ratio_on_single_gold
+OUTPUT_DIR=outputs/search_params/neg_pos_ratio_on_single_gold_w_nc
 mkdir -p ${OUTPUT_DIR}
 pwd >> ${OUTPUT_DIR}/cout
 ls -la >> ${OUTPUT_DIR}/cout
@@ -21,6 +21,6 @@ negative_ratios=(6.0 8.0 10.0 12.0 14.0)
 
 for negative_ratio in ${negative_ratios[@]}; do
     echo "negative_ratio: ${negative_ratio}" >>${OUTPUT_DIR}/cout
-    MAKE="WITH_O=True FIRST_STAGE_CHUNKER=\"enumerated\" POSITIVE_RATIO_THR_OF_NEGATIVE_CAT=1.0 NEGATIVE_RATIO_OVER_POSITIVE=${negative_ratio} make"
+    MAKE="WITH_NEGATIVE_CATEGORIES=True WITH_O=True FIRST_STAGE_CHUNKER=\"enumerated\" POSITIVE_RATIO_THR_OF_NEGATIVE_CAT=1.0 NEGATIVE_RATIO_OVER_POSITIVE=${negative_ratio} make"
     eval ${MAKE} train_on_gold -j$(nproc) >>${OUTPUT_DIR}/cout 2>>${OUTPUT_DIR}/cerr
 done
