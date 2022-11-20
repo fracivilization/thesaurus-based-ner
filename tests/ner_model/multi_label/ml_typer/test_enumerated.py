3252a62c47dbc7a267bf7190854058c5b6df0461
takayoshi-s <fractal.civilization@gmail.com>
2022-11-20 21:04:54 +0900

    MultiLabelEnumeratedTyperとBertForMultiLabelEnumeratedTyperの初期化を単体テストで確認できるようにした

--- tests/ner_model/multi_label/ml_typer/test_enumerated.py
+++ tests/ner_model/multi_label/ml_typer/test_enumerated.py
@@ -1,16 +1,68 @@
-import omegaconf
-from src.ner_model.multi_label.ml_typer.enumerated import MultiLabelTyperConfig
+from src.ner_model.multi_label.ml_typer.enumerated import (
+    MultiLabelEnumeratedModelArguments,
+    MultiLabelEnumeratedTyperConfig,
+    BertForEnumeratedMultiLabelTyper,
+    MultiLabelEnumeratedTyper,
+)
+from transformers import AutoConfig
+import pytest
 
 
-def test_load_multi_label_typer_config():
-    MultiLabelTyperConfig()
+@pytest.fixture
+def model_args() -> MultiLabelEnumeratedModelArguments:
+    return MultiLabelEnumeratedModelArguments(
+        model_name_or_path="dmis-lab/biobert-base-cased-v1.1"
+    )
 
 
-def test_loading_BertForEnumeratedMultiLabelTyper():
-    # TODO: BertForEnumeratedMultiLabelTyper を loadできる
+@pytest.fixture
+def auto_config(model_args) -> AutoConfig:
+
+    task_name = "ner"
+    num_labels = 5
+    return AutoConfig.from_pretrained(
+        model_args.config_name
+        if model_args.config_name
+        else model_args.model_name_or_path,
+        num_labels=num_labels,
+        finetuning_task=task_name,
+        cache_dir=model_args.cache_dir,
+        model_args=model_args,
+    )
     pass
 
 
-def test_loading_MultiLabelEnumeratedTyper():
-    # TODO: MultiLabelEnumeratedTyper を loadできる
-    pass
+@pytest.fixture
+def bert_model(model_args, auto_config) -> BertForEnumeratedMultiLabelTyper:
+    negative_under_sampling_ratio = 0.05
+    return BertForEnumeratedMultiLabelTyper.from_pretrained(
+        model_args,
+        negative_under_sampling_ratio=negative_under_sampling_ratio,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=auto_config,
+        cache_dir=model_args.cache_dir,
+    )
+
+
+class TestBertForEnumeratedMultiLabelTyper:
+    def test_loading_BertForEnumeratedMultiLabelTyper(self, bert_model):
+        pass
+
+
+@pytest.fixture
+def multi_label_enumerated_typer_config() -> MultiLabelEnumeratedTyperConfig:
+    return MultiLabelEnumeratedTyperConfig(
+        train_datasets="fixtures/msmlc_gold_100_times_3"
+    )
+
+
+@pytest.fixture
+def multi_label_enumerated_typer(
+    multi_label_enumerated_typer_config,
+) -> MultiLabelEnumeratedTyper:
+    MultiLabelEnumeratedTyper(multi_label_enumerated_typer_config)
+
+
+class TestMultiLabelEnumeratedTyper:
+    def test_loading_MultiLabelEnumeratedTyper(self, multi_label_enumerated_typer):
+        pass
