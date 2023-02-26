06646f2e250e1083d09247158d217b6869382d38
takayoshi-s <fractal.civilization@gmail.com>
2023-02-26 15:47:01 +0900

    (DICTIONARY_FORM_)TERM2CATSが保存されていれば更新しないようにした

--- makefiles/dependencies.mk
+++ makefiles/dependencies.mk
@@ -144,16 +144,24 @@ $(PSEUDO_MSMLC_DATA_ON_GOLD): $(PSEUDO_MULTI_LABEL_NER_DATA_ON_GOLD)
 	+output_dir=$(PSEUDO_MSMLC_DATA_ON_GOLD)
 
 $(DICTIONARY_FORM_TERM2CATS): $(TERM2CATS_DIR) $(UMLS_DIR)
+ifeq ($(wildcard $(REMAIN_COMMON_SENSE_FOR_TERM2CATS)), "")
 	@echo DICTIONARY_FORM_TERM2CATS: $(DICTIONARY_FORM_TERM2CATS)
 	${PYTHON} -m cli.preprocess.load_dictionary_form_term2cats \
 		--knowledge-base=$(KNOWLEDGE_BASE) \
 		--remain-common-sense=$(REMAIN_COMMON_SENSE_FOR_TERM2CATS) \
 		--output-dir=$(DICTIONARY_FORM_TERM2CATS)
+else
+	echo "既にDICTIONARY_FORM_TERM2CATSは存在します"
+endif
 
 $(TERM2CATS): $(DICTIONARY_FORM_TERM2CATS)
+ifeq ($(wildcard $(TERM2CATS)), "")
 	${PYTHON} -m cli.preprocess.inflect_terms_of_term2cats \
 		--dictionary-form-term2cats-dir=$(DICTIONARY_FORM_TERM2CATS) \
 		--output-dir=$(TERM2CATS)
+else
+	echo "既にTERM2CATSは存在します"
+endif
 
 $(PSEUDO_ON_GOLD_TRAINED_MSMLC_MODEL): $(PSEUDO_MSMLC_DATA_ON_GOLD)
 	$(TRAIN_MSMLC_BASE_CMD) \
