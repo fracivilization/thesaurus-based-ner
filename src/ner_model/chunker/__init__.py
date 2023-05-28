fd4b68fcec25024093d2232467bdcc694ed5cb2c
Takayoshi Shibahara <fractal.civilization@gmail.com>
2023-05-28 10:25:06 +0900

    Flair使ってなかったので消した

--- src/ner_model/chunker/__init__.py
+++ src/ner_model/chunker/__init__.py
@@ -1,13 +1,10 @@
 from .abstract_model import ChunkerConfig, EnumeratedChunker
-from .flair_model import FlairNPChunker
 from .spacy_model import BeneparNPChunker, SpacyNPChunker
 from hydra.core.config_store import ConfigStore
 
 
 def chunker_builder(config: ChunkerConfig):
-    if config.chunker_name == "FlairNPChunker":
-        return FlairNPChunker()
-    elif config.chunker_name == "BeneparNPChunker":
+    if config.chunker_name == "BeneparNPChunker":
         return BeneparNPChunker()
     elif config.chunker_name == "SpacyNPChunker":
         return SpacyNPChunker(config)
@@ -19,15 +16,8 @@ def chunker_builder(config: ChunkerConfig):
 
 def register_chunker_configs(group="ner_model/chunker") -> None:
     cs = ConfigStore.instance()
-    from .flair_model import FlairNPChunkerConfig
     from .spacy_model import BeneparNPChunkerConfig, SpacyNPChunkerConfig
     from .abstract_model import EnumeratedChunkerConfig
-
-    cs.store(
-        group=group,
-        name="FlairNPChunker",
-        node=FlairNPChunkerConfig,
-    )
 
     cs.store(
         group=group,
