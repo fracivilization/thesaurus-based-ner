4179f393b6f42ccde58c040faab629044b0bae7b
Takayoshi Shibahara <fractal.civilization@gmail.com>
2023-09-17 14:51:41 +0900

    HydraAddaptedTrainingArgumentsを修正

--- src/utils/hydra.py
+++ src/utils/hydra.py
@@ -43,6 +43,42 @@ class HydraAddaptedTrainingArguments(OrigTrainingArguments):
         default=None,
         metadata={"help": "The name of the repository to which push the `Trainer`."},
     )
+    # NOTE: 元の型は debug: Union[str, List[DebugOption]] だが UnionにOmegaConfが対応してないので上書きする
+    debug: str = field( 
+        default="",
+        metadata={
+            "help": (
+                "Whether or not to enable debug mode. Current options: "
+                "`underflow_overflow` (Detect underflow and overflow in activations and weights), "
+                "`tpu_metrics_debug` (print debug metrics on TPU)."
+            )
+        },
+    )
+    # NOTE: 元の型は Optional[Union[List[ShardedDDPOption], str]] だが UnionにOmegaConfが対応してないので上書きする
+    sharded_ddp: Optional[str] = field(
+        default="",
+        metadata={
+            "help": (
+                "Whether or not to use sharded DDP training (in distributed training only). The base option should be"
+                " `simple`, `zero_dp_2` or `zero_dp_3` and you can add CPU-offload to `zero_dp_2` or `zero_dp_3` like"
+                " this: zero_dp_2 offload` or `zero_dp_3 offload`. You can add auto-wrap to `zero_dp_2` or `zero_dp_3`"
+                " with the same syntax: zero_dp_2 auto_wrap` or `zero_dp_3 auto_wrap`."
+            ),
+        },
+    )
+    # NOTE: 元の型は Optional[Union[List[FSDPOption], str]] だが UnionにOmegaConfが対応してないので上書きする
+    fsdp: Optional[str] = field(
+        default="",
+        metadata={
+            "help": (
+                "Whether or not to use PyTorch Fully Sharded Data Parallel (FSDP) training (in distributed training"
+                " only). The base option should be `full_shard`, `shard_grad_op` or `no_shard` and you can add"
+                " CPU-offload to `full_shard` or `shard_grad_op` like this: full_shard offload` or `shard_grad_op"
+                " offload`. You can add auto-wrap to `full_shard` or `shard_grad_op` with the same syntax: full_shard"
+                " auto_wrap` or `shard_grad_op auto_wrap`."
+            ),
+        },
+    )
 
     def __post_init__(self):
         """Adapt hydra because some parameters (e.g. debug) is changed in those type
