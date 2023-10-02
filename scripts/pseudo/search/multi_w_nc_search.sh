8fec68b4446716309b1d027aa83533f2a4c523c0
takayoshi-s <fractal.civilization@gmail.com>
2023-10-02 23:29:42 +0900

    EPOCH_NUMを指定しないようにした

--- scripts/pseudo/search/multi_w_nc_search.sh
+++ scripts/pseudo/search/multi_w_nc_search.sh
@@ -1,4 +1,4 @@
 negative_ratios=(0.005 0.01 0.05 0.1 0.3 0.5 1.0 4.0 8.0 16.0)
 for NEGATIVE_RATIO in ${negative_ratios[@]}; do
-    qsub -v EPOCH_NUM=${EPOCH_NUM} -v NEGATIVE_RATIO=${NEGATIVE_RATIO} scripts/pseudo/multi_w_nc.sh
+    qsub -v NEGATIVE_RATIO=${NEGATIVE_RATIO} scripts/pseudo/multi_w_nc.sh
 done
